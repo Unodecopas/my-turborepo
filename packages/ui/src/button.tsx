d47622d15b1eb8a3c3541cd711e5542dfabd4676
jesus.gallardo@petroprix.com-at-658554454469 <jesus.gallardo@petroprix.com-at-658554454469>
2024-11-25 10:36:36 +0100

    feat(ui): cambiado button mensaje

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -12,7 +12,7 @@ export const Button = ({ children, className, appName }: ButtonProps) => {
   return (
     <button
       className={className}
-      onClick={() => alert(`Hola from your ${appName} app!`)}
+      onClick={() => alert(`Hola asd your ${appName} app!`)}
     >
       {children}
     </button>
